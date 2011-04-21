65509937580607ac73add44d3da45fb5d3a0e666
Al Newkirk <we@ana.im>
2011-04-21 11:00:34 -0400

    re-filed perl-critic and perl-tidy

---
+++ perlcritic.pl
@@ -0,0 +1,5 @@
+system 'perlcritic --profile=perlcriticrc ' . $_ for glob './lib/*.pm';
+system 'perlcritic --profile=perlcriticrc ' . $_ for glob './lib/Scrappy/*.pm';
+system 'perlcritic --profile=perlcriticrc ' . $_ for glob './lib/Scrappy/Scraper/*.pm';
+
+print  'critique complete';
---
+++ perltidy.pl
@@ -0,0 +1,5 @@
+system 'perltidy --pro=perltidyrc ' . $_ for glob './lib/*.pm';
+system 'perltidy --pro=perltidyrc ' . $_ for glob './lib/Scrappy/*.pm';
+system 'perltidy --pro=perltidyrc ' . $_ for glob './lib/Scrappy/Scraper/*.pm';
+
+print  'tidy complete';
