0b0d02b02220701d788d4afa2bf877dc40354eb3
Al Newkirk <github@alnewkirk.com>
2011-04-29 17:00:57 -0400

    fixed deployment

--- perlcritic.pl
+++ perlcritic.pl
@@ -1,6 +1,6 @@
-system 'perlcritic --profile=perlcriticrc ' . $_ for glob './lib/*.pm';
-system 'perlcritic --profile=perlcriticrc ' . $_ for glob './lib/Scrappy/*.pm';
-system 'perlcritic --profile=perlcriticrc ' . $_ for glob './lib/Scrappy/Plugin/*.pm';
-system 'perlcritic --profile=perlcriticrc ' . $_ for glob './lib/Scrappy/Scraper/*.pm';
-
-print  'critique complete';
+system "perlcritic --profile=perlcriticrc " . $_ for glob "./lib/*.pm";
+system "perlcritic --profile=perlcriticrc " . $_ for glob "./lib/Scrappy/*.pm";
+system "perlcritic --profile=perlcriticrc " . $_ for glob "./lib/Scrappy/Action/*.pm";
+system "perlcritic --profile=perlcriticrc " . $_ for glob "./lib/Scrappy/Plugin/*.pm";
+system "perlcritic --profile=perlcriticrc " . $_ for glob "./lib/Scrappy/Project/*.pm";
+system "perlcritic --profile=perlcriticrc " . $_ for glob "./lib/Scrappy/Scraper/*.pm";
--- perltidy.pl
+++ perltidy.pl
@@ -1,6 +1,15 @@
-system 'perltidy --pro=perltidyrc ' . $_ for glob './lib/*.pm';
-system 'perltidy --pro=perltidyrc ' . $_ for glob './lib/Scrappy/*.pm';
-system 'perltidy --pro=perltidyrc ' . $_ for glob './lib/Scrappy/Plugin/*.pm';
-system 'perltidy --pro=perltidyrc ' . $_ for glob './lib/Scrappy/Scraper/*.pm';
+my $ver = $ARGV[0] ? "./$ARGV[0]" : ".";
 
-print  'tidy complete';
+print  "... cleaning up $ver";
+
+system "perltidy --pro=perltidyrc " . $_ for glob "$ver/lib/*.pm";
+system "perltidy --pro=perltidyrc " . $_ for glob "$ver/lib/Scrappy/*.pm";
+system "perltidy --pro=perltidyrc " . $_ for glob "$ver/lib/Scrappy/Action/*.pm";
+system "perltidy --pro=perltidyrc " . $_ for glob "$ver/lib/Scrappy/Plugin/*.pm";
+system "perltidy --pro=perltidyrc " . $_ for glob "$ver/lib/Scrappy/Project/*.pm";
+system "perltidy --pro=perltidyrc " . $_ for glob "$ver/lib/Scrappy/Scraper/*.pm";
+
+if ($ver ne '.') {
+    system "rm $ver.tar.gz";
+    system "tar -czf $ver.tar.gz $ver/*";
+}
